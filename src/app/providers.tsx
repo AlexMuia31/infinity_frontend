f9e3b20dc5a1a6e57cb456416e8d7245464daaa5
AlexMuia31 <alexmuia31@gmail.com>
2026-01-03 23:47:50 +0300

    wallet connect with rainbow toolkit

---
+++ src/app/providers.tsx
@@ -0,0 +1,31 @@
+"use client";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, base } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+import { http } from "wagmi";
+
+const config = getDefaultConfig({
+  appName: "My RainbowKit App",
+  projectId: "YOUR_PROJECT_ID",
+  chains: [mainnet, base],
+  transports: {
+    [mainnet.id]: http("https://eth-mainnet.g.alchemy.com/v2/..."),
+    [base.id]: http("https://base-mainnet.g.alchemy.com/v2/..."),
+  },
+});
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
