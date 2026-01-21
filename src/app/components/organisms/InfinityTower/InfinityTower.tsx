9582ee405d019e8775a67c981015ef5cc42001f2
AlexMuia31 <alexmuia31@gmail.com>
2026-01-21 18:56:09 +0300

    added floor proterties to cards

--- src/app/components/organisms/InfinityTower/InfinityTower.tsx
+++ src/app/components/organisms/InfinityTower/InfinityTower.tsx
@@ -2,6 +2,7 @@ import { useAllFloors } from "@/app/hooks/CreateFloor/CreateFloor";
 import { Canvas } from "@react-three/fiber";
 import React from "react";
 import { Floor } from "../../models/Floor";
+import { Card, CardContent, Typography, Box } from "@mui/material";
 
 export function colorToHex(color: bigint): string {
   return `#${color.toString(16).padStart(6, "0")}`;
@@ -9,51 +10,70 @@ export function colorToHex(color: bigint): string {
 
 const InfinityTower = () => {
   const { floors, isLoading, totalFloors } = useAllFloors();
+  const [scrollPosition, setScrollposition] = React.useState(0);
+
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollposition(position);
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   if (isLoading) return <div>Loading floors...</div>;
 
   return (
-    <div id="InfinityTowerCanvasWrapper">
-      <Canvas camera={{ position: [-12, 1, 14], fov: 35, near: 1, far: 100 }}>
-        <ambientLight intensity={0.5} />
-        <pointLight position={[20, 30, 10]} />
-        <pointLight position={[20, 30, 10]} color="blue" />
-        <spotLight position={[-2, -1, 32]} angle={0.2} intensity={1} />
-        {floors.map((floor, index) => (
-          <Floor
-            key={floor.id}
-            position={[0, index * 2, 0]}
-            rotation={[0, Math.PI, index * 0.08]}
-            color={colorToHex(floor.color)}
-            windowsTint={floor.windowsTint.toString()}
-          />
-        ))}
-      </Canvas>
-      {/* <h2>Total Floors: {totalFloors?.toString()}</h2>
-      {floors.map((floor) => (
-        <div
-          key={floor.id}
-          style={{
-            backgroundColor: `#${floor.color.toString(16).padStart(6, "0")}`,
-            padding: "20px",
-            margin: "10px",
-            borderRadius: "8px",
+    <>
+      <div id="InfinityTowerCanvasWrapper">
+        <Canvas camera={{ position: [-12, 1, 14], fov: 35, near: 1, far: 100 }}>
+          <ambientLight intensity={0.5} />
+          <pointLight position={[20, 30, 10]} />
+          <pointLight position={[20, 30, 10]} color="blue" />
+          <spotLight position={[-2, -1, 32]} angle={0.2} intensity={1} />
+          {floors.map((floor, index) => (
+            <Floor
+              key={floor.id}
+              position={[0, index * 2, 0]}
+              rotation={[0, Math.PI, index * 0.08]}
+              color={colorToHex(floor.color)}
+              windowsTint={floor.windowsTint.toString()}
+            />
+          ))}
+        </Canvas>
+      </div>
+      {floors.map((floor, index) => (
+        <Box
+          key={index}
+          sx={{
+            pointerEvents: "none",
+            maxWidth: 1200,
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
           }}
         >
-          <h3>
-            Floor #{floor.id} - {floor.ownerName}
-          </h3>
-          <p>{floor.message}</p>
-          <p>Color: {colorToHex(floor.color)}</p>
-          {floor.link && (
-            <a href={floor.link} target="_blank" rel="noopener noreferrer">
-              Visit Link
-            </a>
-          )}
-          <p>Window Tint: {floor.windowsTint.toString()}%</p>
-        </div>
-      ))} */}
-    </div>
+          <Card sx={{ p: 1, width: "300px", height: "100px", zIndex: 999 }}>
+            <CardContent>
+              <Typography variant="h6">Floor {index + 1}</Typography>
+              <Typography variant="body2">
+                <strong>Owner:</strong> {floor.ownerName}
+              </Typography>
+              <Typography variant="body2">
+                <strong>Message:</strong> {floor.message}
+              </Typography>
+              <Typography variant="body2">
+                <strong>Link</strong> {floor.link}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Box>
+      ))}
+    </>
   );
 };
 
